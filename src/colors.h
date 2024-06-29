da9f74a355feb5a589f4852bf5e5d8ff3b56c860
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-06-29 09:13:23 +0530

    🪴 Add colors

---
+++ src/colors.h
@@ -0,0 +1,13 @@
+/*********************************************************************
+ * colors.h
+ * Macros for ANSI colors.
+ *********************************************************************/
+
+#define DEFAULT "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define WHITE "\033[97m"
+
+#define color(COLOR) printf(COLOR)
